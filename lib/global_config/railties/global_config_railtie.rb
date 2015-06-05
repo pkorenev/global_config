7367ba085bd89f1d7e9d0e029b05f17a1c137301
root <root@node224.hvosting.ua>
2015-06-05 13:00:45 +0300

    check if file exists; add railties dependency to gemspec

--- lib/global_config/railties/global_config_railtie.rb
+++ lib/global_config/railties/global_config_railtie.rb
@@ -3,6 +3,9 @@ class MyCoolRailtie < Rails::Railtie
   # railtie code goes here
   initializer "my_cool_railtie.boot_foo" do
     #Foo.boot(Bar)
-    require "/home/applications/config/smtp_settings.rb"
+    files = %w(/home/applications/config/smtp_settings.rb)
+    files.select{|f| File.exists?(f) }.each do |f|
+    	require f
+	end
   end
 end
